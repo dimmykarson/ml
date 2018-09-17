2e236880951d60cfac7c5c425c3efd8bd3d4f2d5
Dimmy Magalhães <dimmyk@gmail.com>
2018-09-17 14:09:08 -0300

    .

--- trab3/mlp/mlpIMDB.py
+++ trab3/mlp/mlpIMDB.py
@@ -5,11 +5,17 @@ import pandas
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
+from keras.callbacks import History
 from sklearn.datasets import load_svmlight_file
 from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+
+qt_neuronios = 50
+qt_camadas = 1
+n_epocas = 20
 
 
-X_train, y_train = load_svmlight_file('saida.txt')
+X_train, y_train = load_svmlight_file('IMDBtrain.txt')
 X_test, y_test = load_svmlight_file('IMDBtest.txt')
 
 ## save for the confusion matrix
@@ -20,23 +26,37 @@ y_train = keras.utils.to_categorical(y_train, num_classes=2)
 y_test = keras.utils.to_categorical(y_test, num_classes=2)
 
 model = Sequential()
+history = History()
 # Dense(50) is a fully-connected layer with 50 hidden units.
 # in the first layer, you must specify the expected input data shape:
 # here, 100-dimensional vectors.
-model.add(Dense(50, activation='relu', input_dim=100))
+n_neuronios = qt_neuronios
+
+for i in range(0, qt_camadas):
+	model.add(Dense(n_neuronios, activation='relu', input_dim=100))
+	n_neuronios=n_neuronios/2
 model.add(Dense(2, activation='softmax'))
 
 model.compile(loss='binary_crossentropy',
 							optimizer='rmsprop',
 							metrics=['accuracy'])
 
-model.fit(X_train, y_train, validation_split=0.33, epochs=20, batch_size=128)
+model.fit(X_train, y_train, validation_split=0.33, epochs=n_epocas, batch_size=128, callbacks=[history])
 
 score = model.evaluate(X_test, y_test, batch_size=128)
 print (score)
-
 y_pred = model.predict_classes(X_test)
-
 cm = confusion_matrix(label, y_pred)
 print (cm)
+
+print("Ploting")
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plot_name = "mlp_Neuronios_{0}_qt_camadas_{1}_epocas_{2}".format(qt_neuronios, qt_camadas, n_epocas)
+plt.savefig(plot_name)
+plt.show()
 
