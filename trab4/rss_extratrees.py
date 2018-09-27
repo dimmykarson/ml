0d844c49c7e292bad7d6e72e9e607364a4f101f3
DIMMY KARSON SOARES MAGALHAES <dksmagalhaes@inf.ufpr.br>
2018-09-27 14:47:36 -0300

    Default code

--- trab4/rss_extratrees.py
+++ trab4/rss_extratrees.py
@@ -1,5 +1,4 @@
-from sklearn.ensemble import BaggingClassifier
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import ExtraTreesClassifier
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,21 +6,17 @@ from sklearn import model_selection
 from sklearn.datasets import load_svmlight_file
 from sklearn.metrics import confusion_matrix
 
-def run(params):
+def run(params=None):
 
 	X_train, y_train = load_svmlight_file('digTrain20k.txt')
 	X_test, y_test = load_svmlight_file('digTest58k.txt')
 	seed = 7
 
 	kfold = model_selection.KFold(n_splits=10, random_state=seed)
-	clf = DecisionTreeClassifier()
-	num_trees = 2
-	model = BaggingClassifier(base_estimator=clf, n_estimators=num_trees, random_state=seed)
+	model = ExtraTreesClassifier(n_estimators=10)
 
 	results = model_selection.cross_val_score(model, X_train, y_train, cv=kfold)
 	print(results.mean())
-
-
 
 run()
 
